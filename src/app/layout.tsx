2c6af8987ad9ee2477741d8e099cf98f4eb7c636
rats4final <rats4final@gmail.com>
2024-06-26 05:24:59 -0400

    agregado react-select, next-themes y sonner de shad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactQueryClientProvider } from "./react-query-client-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <ReactQueryClientProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <Toaster richColors/>
+        </body>
       </html>
     </ReactQueryClientProvider>
   );
