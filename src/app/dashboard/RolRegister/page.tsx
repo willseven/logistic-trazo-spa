f99e64f09712b7fa8f2d777f46c080e0d02e7ff0
Jose Mendoza <joseemendozaan1@gmail.com>
2024-07-01 14:58:33 -0400

    pagina de roles añadida

---
+++ src/app/dashboard/RolRegister/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { api } from "@/lib/api";
+import { TableUsers } from "@/modules/usersManagement/components/TableUsers";
+import { IUser } from "@/modules/usersManagement/interface/users";
+import { useQuery } from "@tanstack/react-query";
+
+import { useState } from "react";
+import { TableRol } from '../../../modules/RolRegister/components/TableRol';
+
+const RolRegister = () => {
+  const token = localStorage.getItem("token");
+  const id = localStorage.getItem("id");
+  
+  const [pagination, setPagination] = useState({
+    currentPage: 1, 
+    itemsPerPage: 10,
+    totalItems: 0,
+    totalPages: 1,
+  });
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["Rols"],
+    queryFn: async () => {
+      const response = await api.get(`/rol`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+   
+
+      return response.data;
+    },
+    
+    enabled: true,
+    staleTime: 1000 * 60 * 10, // Volver a hacer fetch luego de 10 min
+  });
+  if (isPending) return "Pending...";
+  if (isError) return `Error: ${error.message}`;
+  console.log(data)
+
+  return (
+    <div>
+      <h1 className="flex items-center justify-center font-bold text-xl">Roles</h1>
+      <section>
+        <TableRol
+          data={data ?? []} 
+          pagination={pagination} 
+          setPagination={setPagination} 
+        />
+      </section>
+    </div>
+  );
+};
+
+export default RolRegister;
---
+++ src/modules/RolRegister/interface/rols.ts
@@ -0,0 +1,7 @@
+export interface IRol{
+    id: number,
+    name: string,
+    label: string,
+    main: string,
+    icon: string
+}
