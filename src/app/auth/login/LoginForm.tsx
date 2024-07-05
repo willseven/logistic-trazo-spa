7c860c187c1baa19c8ba254cb93fbee77778751c
rats4final <rats4final@gmail.com>
2024-07-04 22:46:15 -0400

    mejorado estilos del login

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -16,16 +16,15 @@ import {
 import { Input } from "@/components/ui/input";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import {useUserStore} from "@/lib/store";
+import { useUserStore } from "@/lib/store";
 import { UserResponse } from "@/lib/types";
 import { api } from "@/lib/api";
+import Image from "next/image";
 
 const loginFormSchema = z.object({
   usernameOrEmail: z.string(),
   password: z.string(),
 });
-
-
 
 type LoginForm = z.infer<typeof loginFormSchema>;
 
@@ -50,12 +49,11 @@ export default function LoginForm() {
       const user: UserResponse = response.data;
       const id = user.user.id;
       console.log(user.rols);
-      
 
       //poner la store
 
-      useUserStore.getState().setUserResponse(user)
-      useUserStore.getState().setRoles(user.rols)
+      useUserStore.getState().setUserResponse(user);
+      useUserStore.getState().setRoles(user.rols);
       localStorage.setItem("token", user.token);
       localStorage.setItem("id", id.toString());
       const firstRole = user.rols[0];
@@ -63,10 +61,9 @@ export default function LoginForm() {
 
       const roleMenuResponse = await api.get(`/Rol/${firstRole.id}/Menu`, {
         headers: {
-          "Authorization": `Bearer ${user.token}`, // Ejemplo de un header adicional
-        }
+          Authorization: `Bearer ${user.token}`, // Ejemplo de un header adicional
+        },
       });
-
 
       useUserStore.getState().setMenuList(roleMenuResponse.data.menuList);
       // router.push(`../dashboard/${roleMenuResponse.data.menuList[0].name}`);
@@ -77,47 +74,89 @@ export default function LoginForm() {
   }
 
   return (
-    <div className="border p-5 rounded-lg">
+    <div className="bg-primary rounded-lg shadow-lg p-8">
+      <div className="flex items-center justify-between mb-6">
+        <h1 className="text-3xl font-bold text-primary-foreground">
+          Trazo Logistico
+        </h1>
+        <div>
+          <Image src="/images/noxun.jpg" width={200} height={100} alt="logo" />
+        </div>
+      </div>
       <Form {...loginForm}>
-        <form onSubmit={loginForm.handleSubmit(onSubmit)}>
-          <FormField
-            control={loginForm.control}
-            name="usernameOrEmail"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Nombre de Usuario</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="Introduzca su nombre de usuario"
-                    {...field}
-                  />
-                </FormControl>
-                <FormDescription>Su nombre de usuario</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={loginForm.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Contraseña</FormLabel>
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="Introduzca su nombre contraseña"
-                    {...field}
-                  />
-                </FormControl>
-                <FormDescription>Su contraseña</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Ingresar</Button>
+        <form className="space-y-4" onSubmit={loginForm.handleSubmit(onSubmit)}>
+          <div>
+            <FormField
+              control={loginForm.control}
+              name="usernameOrEmail"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-primary-foreground">
+                    Nombre de Usuario
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-primary-foreground text-primary rounded-md"
+                      placeholder="Introduzca su nombre de usuario"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>Su nombre de usuario</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div>
+            <FormField
+              control={loginForm.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-primary-foreground">
+                    Contraseña
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-primary-foreground text-primary rounded-md"
+                      type="password"
+                      placeholder="Introduzca su nombre contraseña"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>Su contraseña</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button
+            className="w-full bg-primary-foreground text-primary hover:bg-accent hover:text-accent-foreground"
+            type="submit"
+          >
+            Ingresar
+          </Button>
         </form>
       </Form>
     </div>
+  );
+}
+
+function MountainIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+    </svg>
   );
 }
