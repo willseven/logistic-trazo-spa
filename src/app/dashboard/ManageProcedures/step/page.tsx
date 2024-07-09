be3a949dfad0475afc961ccac0b12721b34b853c
Jose Mendoza <joseemendozaan1@gmail.com>
2024-07-09 11:31:46 -0400

    suspense añadido

--- src/app/dashboard/ManageProcedures/step/page.tsx
+++ src/app/dashboard/ManageProcedures/step/page.tsx
@@ -1,15 +1,16 @@
 "use client";
+import { Suspense } from "react";
 import { api } from "@/lib/api";
 import { useUserStore } from "@/lib/store";
 import { TableProcedure } from "@/modules/ManageProcedures/components/Tableprocedures";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter, useSearchParams } from "next/navigation";
 
-export default function ManageProcedures() {
+function ManageProceduresComponent() {
   let token: string | null = null;
   let id: string | null = null;
   const router= useRouter();
-  const params = useSearchParams()
+  const params = useSearchParams();
   const step = params.get("step");
   const typeid = params.get("id");
   const currentRoleId = params.get("currentRoleId");
@@ -17,13 +18,13 @@ export default function ManageProcedures() {
   const { currentRole } = useUserStore((state) => ({
     currentRole: state.currentRole,
   }));
-  console.log(currentRole)
+  
   if (typeof window !== "undefined") {
     token = window.localStorage.getItem("token");
     id = window.localStorage.getItem("id");
   }
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ["stepprocedure"],
     queryFn: async () => {
       const response = await api.get(`/procedure/bystepbyproceduretypeid/${step}/proceduretypeid/${typeid}/rol/${currentRoleId}`, {
@@ -32,30 +33,33 @@ export default function ManageProcedures() {
         },
       });
 
-   
-
       return response.data;
     },
-    
     enabled: true,
     staleTime: 1000 * 60 * 10, // Volver a hacer fetch luego de 10 min
   });
-  if (isPending) return "Pending...";
+
+  if (isLoading) return "Loading...";
   if (isError) return `Error: ${error.message}`;
-  console.log(data);
-  
-
-
 
   return (
     <div>
-    <h1 className="flex items-center justify-center font-bold text-xl">Roles</h1>
-    <section>
-      <TableProcedure
-        data={data ?? []} 
-      />
-    </section>
-  </div>
-
+      <h1 className="flex items-center justify-center font-bold text-xl">Roles</h1>
+      <section>
+        <TableProcedure data={data ?? []} />
+      </section>
+    </div>
   );
-};
+}
+
+function Loading() {
+  return <div>Loading...</div>;
+}
+
+export default function ManageProcedures() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <ManageProceduresComponent />
+    </Suspense>
+  );
+}
